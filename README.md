e75379d1accf2dec1418341b5dcc5bc148a543eb
Marija <radovicmarija14@gmail.com>
2019-12-01 19:26:45 +0100

    Drugacije sam pristupila pri pravljenju zgrada. Sada se na pritisak dugmica q,e kamera rotira. Popravljeni bagovi. Treba popraviti kretanje kamere koje posle neke razdaljine ima neuobicajeno ponasanje. Dodati osvetljenje sledece nedelje.

--- README.md
+++ README.md
@@ -1,2 +1,6 @@
-# RG135-rooftop-runner
-rooftop-runner
+## RG135-rooftop-runner
+#Pomozi čikici da preskoči sve zgrade!
+_Obrati pažnju na to da zgrade nisu podjednako udaljene međusobno!_
+
+`make`
+`./main`
--- main.c
+++ main.c
@@ -4,24 +4,35 @@
 #include<time.h>
 #include<stdio.h>
 
+#define VELICINAZGRADE 3
+#define BROJZGRADA 50
+#define MANJARAZDALJINA 3
+#define VECARAZDALJINA 6
 #define TIMER0 0
 
 static int window_width, window_height;
 
 #define pi M_PI
 
-static float phi, theta;
+static float theta;
 
 static float delta_phi, delta_theta;
 
 int timer0Active;
-int pozicija;
+int trenutnaZKoordinata;
+int sumaRazdaljina;
+int poslednjaZ;
 
 static void on_timer(int value);
 static void on_keyboard(unsigned char key, int x, int y);
 static void on_reshape(int width, int height);
 static void on_display(void);
+void nadjiRazdaljine();
+void iscrtajOse();
+void iscrtajZgrade();
+void iscrtajKraj();
 
+int razdaljine[BROJZGRADA];
 
 int main(int argc, char **argv){
     glutInit(&argc, argv);
@@ -41,13 +52,14 @@ int main(int argc, char **argv){
     glEnable(GL_DEPTH_TEST);
     glLineWidth(2);
     
-    phi = theta = 0.0001;
+    theta = 0.0001;
     delta_phi = delta_theta = pi / 90;
     
-    pozicija = -5;
+    trenutnaZKoordinata = 2;
 
 	timer0Active = 0;
 
+    nadjiRazdaljine();
     glutMainLoop();
 
     return 0;
@@ -57,11 +69,9 @@ int main(int argc, char **argv){
 static void on_timer(int value){
     //TIMER JE ONAJ KOJI MI POMERA POZICIJU KAMERE 
     if (value == TIMER0){
-		pozicija += 0.5;
+		trenutnaZKoordinata += 1;
 		glutPostRedisplay();
-
-		/* Po potrebi se ponovo postavlja tajmer. */
-		if (timer0Active)
+    if (timer0Active && trenutnaZKoordinata<poslednjaZ-5)
 		    glutTimerFunc(100, on_timer, TIMER0);
 	}
 }
@@ -102,7 +112,6 @@ static void on_keyboard(unsigned char key, int x, int y){
 }
 
 static void on_reshape(int width, int height){
-    /* Pamte se sirina i visina prozora. */
     window_width = width;
     window_height = height;
 }
@@ -115,25 +124,59 @@ static void on_display(void){
     glMatrixMode(GL_PROJECTION);
     glLoadIdentity();
     gluPerspective(
-            60,
+            80,
             window_width/(float)window_height,
-            1, 10);
+            1, 30);
 
     glMatrixMode(GL_MODELVIEW);
     glLoadIdentity();
              
-    /* gluLookAt(
-            0, 6, pozicija,
-            0, 5, pozicija + 5,
-            0, 0, 1
-        );
-    */
-
-    gluLookAt(pozicija*sin(theta),6,pozicija*cos(theta),
-            0,5,pozicija + 5,
+    gluLookAt(trenutnaZKoordinata*sin(theta),12,trenutnaZKoordinata*cos(theta),
+            0,10,trenutnaZKoordinata + 2*VELICINAZGRADE,
             0,1,0);
         
      
+    iscrtajOse();
+    iscrtajZgrade();
+    iscrtajKraj();
+
+    glutSwapBuffers();
+}
+
+void nadjiRazdaljine(){
+    int i = 0;
+    int randR = 0;
+    for(i = 0;i<BROJZGRADA;i++){
+        randR = rand()%2;
+        if(randR == 0){
+            razdaljine[i] = MANJARAZDALJINA;
+            sumaRazdaljina+=MANJARAZDALJINA;
+        }
+        else{
+            razdaljine[i] = VECARAZDALJINA;
+            sumaRazdaljina+=VECARAZDALJINA;
+        }
+    }
+    poslednjaZ = (BROJZGRADA+1)*VELICINAZGRADE + sumaRazdaljina+1;
+}
+
+void iscrtajZgrade(){
+    int i;
+    int prethodnaDaljaStranica = razdaljine[0]+VELICINAZGRADE;
+    int trenutnaBlizaStranica = razdaljine[0];
+    for(i = 0;i<BROJZGRADA;i++){
+        trenutnaBlizaStranica = prethodnaDaljaStranica + razdaljine[i];
+        glPushMatrix();
+        glColor3f(1,0, 1);
+        glScalef(1, 5, 1);
+        glTranslatef(0, 0, trenutnaBlizaStranica);
+        glutWireCube(VELICINAZGRADE);
+        prethodnaDaljaStranica = trenutnaBlizaStranica + VELICINAZGRADE; 
+        glPopMatrix();
+    }
+}
+
+void iscrtajOse(){
     glBegin(GL_LINES);
     	glColor3f(0,0,1);
     	//z - osa plava
@@ -152,23 +195,17 @@ static void on_display(void){
     	glVertex3f(0,0,0);
     	glVertex3f(100,0,0);
     glEnd();
-
-//generisanje zgrada
-    
-    int preth = 0;
-    for(int i = 0;i<100;i+=2){
-    	//generisem razdaljinu
-    	int r = rand() % 2;
-    	//fprintf(stderr,"%d\n",r);
-		glPushMatrix();
-			glColor3f(10,0, 10);
-			glScalef(1, 10, 1);
-            glTranslatef(0, 0, preth + i);
-            preth += r;
-			glutWireCube(1);
-		glPopMatrix();
-    }
-
-    /* Nova slika se salje na ekran. */
-    glutSwapBuffers();
 }
+
+void iscrtajKraj(){
+    
+    glBegin(GL_POLYGON);
+        glColor3f(0,15,0);
+        glVertex3f(-100,100,(BROJZGRADA+1)*VELICINAZGRADE + sumaRazdaljina+1);
+        glVertex3f(100,100,(BROJZGRADA+1)*VELICINAZGRADE + sumaRazdaljina+1);
+        glVertex3f(100,-100,(BROJZGRADA+1)*VELICINAZGRADE + sumaRazdaljina+1);
+        glVertex3f(-100,-100,(BROJZGRADA+1)*VELICINAZGRADE + sumaRazdaljina+1);
+    glEnd();
+}
+
+
